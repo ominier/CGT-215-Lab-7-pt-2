a253147236980ab12a6d298a1b161dea4a8c174e
ominier <ominier@purdue.edu>
2024-10-09 13:32:19 -0400

    initial code

--- CGT 215 Lab 7.cpp
+++ CGT 215 Lab 7.cpp
@@ -2,10 +2,48 @@
 //
 
 #include <iostream>
-
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+using namespace std;
+using namespace sf;
+using namespace sfp;
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Create our window and world with gravity 0,1
+	RenderWindow window(VideoMode(800, 600), "Bounce");
+	World world(Vector2f(0, 1));
+	// Create the ball
+	PhysicsCircle ball;
+	ball.setCenter(Vector2f(400, 300));
+	ball.setRadius(20);
+	world.AddPhysicsBody(ball);
+	// Create the floor
+	PhysicsRectangle floor;
+	floor.setSize(Vector2f(800, 20));
+	floor.setCenter(Vector2f(400, 590));
+	floor.setStatic(true);
+	world.AddPhysicsBody(floor);
+	int thudCount(0);
+	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+	Clock clock;
+	Time lastTime(clock.getElapsedTime());
+	while (true) {
+		// calculate MS since last frame
+		Time currentTime(clock.getElapsedTime());
+		Time deltaTime(currentTime - lastTime);
+		int deltaTimeMS(deltaTime.asMilliseconds());
+		if (deltaTimeMS > 0) {
+			world.UpdatePhysics(deltaTimeMS);
+			lastTime = currentTime;
+		}
+		window.clear(Color(0, 0, 0));
+		window.draw(ball);
+		window.draw(floor);
+		window.display();
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
