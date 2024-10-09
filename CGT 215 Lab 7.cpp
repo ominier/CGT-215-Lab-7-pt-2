97c893914243c4a22087ccf90dd9e03de59e2a1c
ominier <ominier@purdue.edu>
2024-10-09 14:01:47 -0400

    final code

--- CGT 215 Lab 7.cpp
+++ CGT 215 Lab 7.cpp
@@ -7,43 +7,119 @@
 using namespace std;
 using namespace sf;
 using namespace sfp;
+
 int main()
 {
 	// Create our window and world with gravity 0,1
 	RenderWindow window(VideoMode(800, 600), "Bounce");
 	World world(Vector2f(0, 1));
-	// Create the ball
+
+	// Ball
 	PhysicsCircle ball;
-	ball.setCenter(Vector2f(400, 300));
+	ball.setCenter(Vector2f(200, 300));
 	ball.setRadius(20);
+	ball.setVelocity(Vector2f(0.5, 0.5)); // Setting initial velocity
 	world.AddPhysicsBody(ball);
-	// Create the floor
+
+	// Floor
 	PhysicsRectangle floor;
 	floor.setSize(Vector2f(800, 20));
 	floor.setCenter(Vector2f(400, 590));
 	floor.setStatic(true);
 	world.AddPhysicsBody(floor);
+
+	// Left wall
+	PhysicsRectangle leftWall;
+	leftWall.setSize(Vector2f(20, 600));
+	leftWall.setCenter(Vector2f(10, 300));
+	leftWall.setStatic(true);
+	world.AddPhysicsBody(leftWall);
+
+	// Right wall
+	PhysicsRectangle rightWall;
+	rightWall.setSize(Vector2f(20, 600));
+	rightWall.setCenter(Vector2f(790, 300));
+	rightWall.setStatic(true);
+	world.AddPhysicsBody(rightWall);
+
+	// Ceiling
+	PhysicsRectangle ceiling;
+	ceiling.setSize(Vector2f(800, 20));
+	ceiling.setCenter(Vector2f(400, 10));
+	ceiling.setStatic(true);
+	world.AddPhysicsBody(ceiling);
+
+	// Center box
+	PhysicsRectangle centerBox;
+	centerBox.setSize(Vector2f(100, 100));
+	centerBox.setCenter(Vector2f(400, 300));
+	centerBox.setStatic(true);
+	world.AddPhysicsBody(centerBox);
+
+	// Counting collisions
 	int thudCount(0);
+	int bangCount(0);
+
+	// Collision responses
 	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
 		cout << "thud " << thudCount << endl;
 		thudCount++;
 	};
+	leftWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+	rightWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+	ceiling.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+	centerBox.onCollision = [&bangCount](PhysicsBodyCollisionResult result) {
+		bangCount++;
+		cout << "bang " << bangCount << endl;
+		if (bangCount >= 3) {
+			cout << "Exiting after 3 bangs on the center box" << endl;
+			exit(0);
+		}
+	};
+
 	Clock clock;
 	Time lastTime(clock.getElapsedTime());
+	
 	while (true) {
 		// calculate MS since last frame
 		Time currentTime(clock.getElapsedTime());
 		Time deltaTime(currentTime - lastTime);
 		int deltaTimeMS(deltaTime.asMilliseconds());
+
 		if (deltaTimeMS > 0) {
 			world.UpdatePhysics(deltaTimeMS);
 			lastTime = currentTime;
 		}
+
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+		}
+
 		window.clear(Color(0, 0, 0));
+
 		window.draw(ball);
 		window.draw(floor);
+		window.draw(leftWall);
+		window.draw(rightWall);
+		window.draw(ceiling);
+		window.draw(centerBox);
+
 		window.display();
 	}
+
+	return(0);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
